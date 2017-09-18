cea5788613a54e215f72c36d752e075bd915fe04
John Michael <john.parsons85210@gmail.com>
2017-09-17 17:06:28 -0700

    add word_check method

--- anagram.rb
+++ anagram.rb
@@ -4,48 +4,34 @@
 # https://www.learnhowtoprogram.com/ruby
 # John Michael - github.com/Johnmichael
 
-def anagrams?(str1, str2)
-  str1.delete(' ').downcase.chars.sort == str2.delete(' ').downcase.chars.sort
+class AnagramCheck
+
+  def word_check(str1, str2)
+    if str1.scan(/[aeoui]/).count != 0 &&  if str2.scan(/[aeoui]/).count != 0
+        puts('these are words')
+      else
+        puts ('one or more inputs was not a word')
+      end
+    end
+  end
+
+  def palindrome?(str1, str2)
+    if str1.delete(' ').downcase.chars == str2.delete(' ').downcase.chars.reverse
+      puts('these words are palindromes')
+   else
+     puts('these are not palindromes')
+   end
+  end
+
+  def anagrams?(str1, str2)
+   if str1.delete(' ').downcase.chars.sort == str2.delete(' ').downcase.chars.sort
+     puts('these words are anagrams')
+   else
+     puts('these words are not anagrams')
+   end
+  end
+
+
+
+
 end
-
-
-# testing space removal
-def removespace!(word)
-  word.delete(' ')
-end
-
-
-
-
-
-
-# Working as intended #
-
-# str1 = "dog"
-# str2 = "cat"
-# => false
-
-# str1 = "astronomer"
-# str2 = "moonstarer"
-# => true
-
-#str1 = "ASTRONOMER"
-#str2 = "moonstarer"
-# => true
-
-#str1 = "astro nomer"
-#str2 = "moonstarer"
-# => true
-
-## End ##
-
-
-
-
-## BUGS ##
-
-# str1 = "astronomer!"
-# str2 = "moonstarer"
-# => false
-
-## End ##
